49cfed198cc8e4a7b21ce1080000e1f946bba113
Prem Raj <premraj299341@gmail.com>
2026-08-25 14:32:50 +0530

    feat: initialize backend project structure with Express and WebSocket support

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,4 @@
+import 'tsconfig-paths/register';
 import { app } from './app';
 import { NODE_ENV, PORT } from '@/core/config/constants';
 import { setupLogWebSocket } from './modules/log/log.service';
